45f369fb66daf2f2875240902a570fe1e62e4715
addevelop <adrienpro.rodrigues@gmail.com>
2024-09-27 15:33:33 +0200

    editurldomaine

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'adrien-rodrigues.fr', // Remplacez par votre domaine
+    siteUrl: 'https://adrien-rodrigues.fr', // Remplacez par votre domaine
     generateRobotsTxt: true, // (facultatif) Génère également un fichier robots.txt
     // Autres options...
   };
