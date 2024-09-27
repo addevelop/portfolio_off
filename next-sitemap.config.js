4a1be89de361b6aa17424cc883e25e861a93ac37
addevelop <adrienpro.rodrigues@gmail.com>
2024-09-27 14:22:25 +0200

    sitemapIndex

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: '127.0.0.1:3000', // Remplacez par votre domaine
+    generateRobotsTxt: true, // (facultatif) Génère également un fichier robots.txt
+    // Autres options...
+  };
